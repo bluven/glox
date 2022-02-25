ee9698310c5a9cf34fc2c118193b751513516fbb
bluven <yanshiyi1983@gmail.com>
2022-02-25 10:06:40 +0800

    add initial constant implementation

--- chunk.go
+++ chunk.go
@@ -3,13 +3,17 @@ package main
 import "fmt"
 
 type OpCode uint
+type Value float64
 
 const (
 	OpReturn OpCode = iota
+	OpConstant
 )
 
 type Chunk struct {
-	codes []OpCode
+	codes     []OpCode
+	constants []Value
+	lines     []int
 }
 
 func NewChunk() *Chunk {
@@ -20,10 +24,18 @@ func NewChunk() *Chunk {
 
 func (c *Chunk) init() {
 	c.codes = nil
+	c.lines = nil
+	c.constants = nil
 }
 
-func (c *Chunk) Write(op OpCode) {
+func (c *Chunk) Write(op OpCode, line int) {
 	c.codes = append(c.codes, op)
+	c.lines = append(c.lines, line)
+}
+
+func (c *Chunk) AddConstant(value Value) int {
+	c.constants = append(c.constants, value)
+	return len(c.constants) - 1
 }
 
 func (c *Chunk) Free() {
@@ -39,18 +51,38 @@ func (c *Chunk) DisassembleChunk(name string) {
 
 func (c *Chunk) disassembleInstruction(offset int) int {
 	fmt.Printf("%04d ", offset)
+	if offset > 0 && c.lines[offset] == c.lines[offset-1] {
+		fmt.Printf("   | ")
+	} else {
+		fmt.Printf("%4d ", c.lines[offset])
+	}
 
 	op := c.codes[offset]
 	switch op {
 	case OpReturn:
-		return simpleInstruction("OP_RETURN", offset)
+		return c.simpleInstruction("OP_RETURN", offset)
+	case OpConstant:
+		return c.constantInstruction("OP_CONSTANT", offset)
 	default:
 		fmt.Printf("Unknown opcode %d\n", op)
 		return offset + 1
 	}
 }
 
-func simpleInstruction(name string, offset int) int {
+func (c *Chunk) simpleInstruction(name string, offset int) int {
 	fmt.Printf("%s\n", name)
 	return offset + 1
+}
+
+func (c *Chunk) constantInstruction(name string, offset int) int {
+	ci := c.codes[offset+1]
+	fmt.Printf("%-16s %4d '", name, ci)
+	printValue(c.constants[ci])
+	fmt.Printf("'\n")
+
+	return offset + 2
+}
+
+func printValue(value Value) {
+	fmt.Printf("%g", value)
 }
--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 func main() {
 	chunk := NewChunk()
-	chunk.Write(OpReturn)
+	ci := chunk.AddConstant(1.2)
+	chunk.Write(OpConstant, 123)
+	chunk.Write(OpCode(ci), 123)
+	chunk.Write(OpReturn, 123)
 	chunk.DisassembleChunk("chunk")
 	chunk.Free()
 }
